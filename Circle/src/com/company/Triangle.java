6697dfd8d7bbc23eb6dfa3ea20e3d783df745aec
Sur1379 <Suren99999@mail.ru>
2021-04-26 18:54:09 +0400

    Triangle.java

---
+++ Circle/src/com/company/Triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+          public class Triangle {
+    public static void main(String[] args) {
+        int a = 3;
+            int b = 4;
+                int c = 5;
+                 int S = (a * b)/2;
+                  System.out.println(S);
+        int L = a + b + c;
+        System.out.println(L);
+    }
+}
