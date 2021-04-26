474adfa8d0069f433deb9a5dd88e7e37df78afff
Sur1379 <Suren99999@mail.ru>
2021-04-26 17:02:33 +0400

    Circle

---
+++ Circle/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   double p = 3.14;
+	    int radius = 3;
+         double area = p * radius * radius;
+           System.out.println(area);
+
+           double perimetre = 2 * p * radius;
+        System.out.println(perimetre);
+    }
+}
